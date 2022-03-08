0d84192398e0116a83a402c4b68d25ac4fee36a5
Priyadharshini G <priy3020@global.chainsys.com>
2022-03-08 12:19:58 +0530

    include challenge3

---
+++ src/advice.js
@@ -0,0 +1,6 @@
+/* eslint-disable semi */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
+const adviceBtn = document.querySelector(".advice-btn");
+const adviceId = document.querySelector(".advice-Id");
+const advicetext = document.querySelector(".advice-text");
